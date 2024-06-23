c24c1ce66806bbf6289e132c3e293335a42a2235
AngelPuentesDevv <apuentest@uniempresarial.edu.co>
2024-06-22 21:42:24 -0500

    Se ha añadido un contador de intentos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Declaración de variables
 let numeroSecreto = 5;
 let numeroUsuario = 0;
+let intentos = 1;
 
 //Uso de ciclo para que el jugador tenga intentos ilimitados
 while (numeroSecreto != numeroUsuario) {
@@ -11,7 +12,9 @@ while (numeroSecreto != numeroUsuario) {
   if (numeroUsuario == numeroSecreto) {
     //Uso de template string para combinar strings con variables
     //La condición se cumple
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
+    alert(
+      `Acertaste, el número es: ${numeroUsuario} ¡Lo acertaste en ${intentos} intentos!`
+    );
   } else {
     if (numeroUsuario > numeroSecreto) {
       alert("El número secreto es menor");
@@ -20,5 +23,7 @@ while (numeroSecreto != numeroUsuario) {
     }
     //La condición no se cumple
     //alert("No has acertado el número");
+    //Se incrementa el contador cuando no se acierta el número
+    intentos++;
   }
 }
