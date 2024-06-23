904557b49673176e9913370eea25cce261b27502
AngelPuentesDevv <apuentest@uniempresarial.edu.co>
2024-06-22 21:26:22 -0500

    se ha agregado un número de intentos ilimitados

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 //Declaración de variables
 let numeroSecreto = 5;
-let numeroUsuario = prompt("Indica un número entre 1 y 10: ");
+let numeroUsuario = 0;
 
-//Declaración de condición
-console.log(numeroUsuario);
-if (numeroUsuario == numeroSecreto) {
-  //Uso de template string para combinar strings con variables
-  //La condición se cumple
-  alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-  if (numeroUsuario > numeroSecreto) {
-    alert("El número secreto es menor");
+//Uso de ciclo para que el jugador tenga intentos ilimitados
+while (numeroSecreto != numeroUsuario) {
+  numeroUsuario = prompt("Indica un número entre 1 y 10: ");
+
+  //Declaración de condición
+  console.log(numeroUsuario);
+  if (numeroUsuario == numeroSecreto) {
+    //Uso de template string para combinar strings con variables
+    //La condición se cumple
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
   } else {
-    alert("El número secreto es mayor");
+    if (numeroUsuario > numeroSecreto) {
+      alert("El número secreto es menor");
+    } else {
+      alert("El número secreto es mayor");
+    }
+    //La condición no se cumple
+    //alert("No has acertado el número");
   }
-  //La condición no se cumple
-  //alert("No has acertado el número");
 }
